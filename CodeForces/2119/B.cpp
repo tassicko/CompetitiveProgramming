4080cc87451ffbe7e146d595a1aa0392b6c9cce2
tassicko <tasik.islam90@gmail.com>
2025-07-08 14:32:36 +0600

    update

--- CodeForces/2119/B.cpp
+++ CodeForces/2119/B.cpp
@@ -13,9 +13,11 @@ void solve(){
         mx = max(mx, x);
     }
     
+    //here let both side is sqared 
     ll dist = (px-qx)*(px-qx) + (py-qy)*(py-qy);
     ll mn = max(2*mx - sum, 0ll);
-
+    // min value can be -ve but (mn*mn) is positive that 
+    // gives wrong ans (mn*mn > dist) so max(0LL, -ve value) is imp.
     
     if((px == qx) && (py == qy)){
         if(mn*mn > dist){
