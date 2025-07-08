1c8690e37fa8882c4ac6f4684bbdb99631c87e3b
tassicko <tasik.islam90@gmail.com>
2025-07-08 14:37:28 +0600

    update

--- CodeForces/2119/B.cpp
+++ CodeForces/2119/B.cpp
@@ -47,3 +47,35 @@ int main(){
     
     return 0;
 }
+/*
+efficient solve:
+
+        void solve(){
+            ll a, px, py, qx, qy;
+            cin >> a >> px >> py >> qx >> qy;
+            ll sum =0, mx =0;
+
+            for(int i=0 ; i<a ; i++){
+                ll x; cin>>x;
+                sum+=x;
+                mx = max(mx, x);
+            }
+            
+            //here let both side is sqared 
+            ll dist = (px-qx)*(px-qx) + (py-qy)*(py-qy);
+            ll mn = max(2*mx - sum, 0ll);
+            // min value can be -ve but (mn*mn) is positive that 
+            // gives wrong ans (mn*mn > dist) so max(0LL, -ve value) is imp.
+            
+            if(s*s < dist){
+                cout<< "NO" << endl;
+                return;
+            }
+            if(mn*mn > dist){
+                cout<< "NO" << endl;
+                return;
+            }
+            cout << "YES" << endl;
+
+        }
+*/
